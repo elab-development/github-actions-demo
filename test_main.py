055c8e41c67b5750c7c37b1eb21c70f2a3fc7403
Aleksandar Joksimovic <93257871+0joksa0@users.noreply.github.com>
2026-02-18 01:19:49 +0100

    Fix test for HTML response

--- test_main.py
+++ test_main.py
@@ -3,18 +3,14 @@ from main import app
 
 client = TestClient(app)
 
-def test_root():
+def test_homepage():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json()["message"] == "CI/CD radi!"
+    assert "CI/CD Demo" in response.text
 
 def test_add():
     response = client.get("/add?a=2&b=3")
     assert response.status_code == 200
     assert response.json()["result"] == 5
 
-def test_homepage():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert "CI/CD Demo" in response.text
 
